23d91087cc504f7188290ee5713d228f374579a9
Miguel Lobato <masg.lobato@gmail.com>
2026-02-18 16:11:56 +0000

    modified:   test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-a = 1
+def conta_palavras(string):
+    res = {}
+    lista = string.split()
+    for palavra in lista:
+        cont = lista.count(palavra)
+        res[palavra] = cont
+    return res
